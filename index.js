4623d3eceb357a9ca70da27683d8c366e004c796
explooosion <ta7382@gmail.com>
2019-10-31 19:18:00 +0800

    Refactor code

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const fs = require('fs')
-const Octokit = require('@octokit/rest')
+const fs = require('fs');
+const Octokit = require('@octokit/rest');
 
 const octokit = new Octokit({
   auth: process.env.ACCESS_TOKEN
 });
 
-// octokit.authenticate({
-//   token: process.env.ACCESS_TOKEN
-// })
+let evetJSON = {};
 
-async function onIssueComment() {
-  console.log('GITHUB_EVENT_PATH', process.env.GITHUB_EVENT_PATH)
-  const evetJSON = await JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'))
-  console.log('Payload', evetJSON)
+/**
+ * Comment to issue
+ * @param {object} payload
+ */
+async function onIssueComment(payload) {
+  return await octokit.issues.createComment(payload)
+    .then(({ status }) => status)
+    .catch(({ status }) => status);
+}
 
-  const OWNER = evetJSON.repository.owner.login;
-  const REPOSITORY = evetJSON.repository.name;
-  const NUMBER = evetJSON.issue.number;
+/**
+ * Get issue participants
+ * @param {object} payload
+ * @param {number} issue_number
+ */
+async function onIssueGetUsers(payload, issue_number) {
+  return await octokit.issues.listCommentsForRepo(payload)
+    .then(({ data }) =>
+      data
+        .filter(({ issue_url }) => String(issue_url).split('/issues/')[1] === String(issue_number))
+        .map(({ user }) => `@${user.login}`)
+        .filter((user, index, arr) => arr.indexOf(user) === index)
+        .join(' '));
+}
+
+async function work() {
+  evetJSON = await JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+  console.log('GITHUB_EVENT_PATH', process.env.GITHUB_EVENT_PATH);
+  console.log('Payload', evetJSON);
+
+  const owner = evetJSON.repository.owner.login;
+  const repo = evetJSON.repository.name;
+  const issue_number = evetJSON.issue.number;
   const BODY = evetJSON.comment.body;
 
-  console.log('Comment Payload', BODY);
-
-  const payload = {
-    owner: OWNER,
-    repo: REPOSITORY,
-    issue_number: NUMBER,
-    body: 'tag somebody.',
-  };
+  const users = await onIssueGetUsers({ owner, repo }, issue_number);
 
   if (String(BODY).includes('/tagall')) {
-    //add a comment to the issue
-    await octokit.issues.createComment(payload)
-      .then(({ data, headers, status }) => {
-        // handle data
-        console.log('BOT has send message.')
-      })
+    const payload = {
+      owner,
+      repo,
+      issue_number,
+      body: `${users}${process.env.MEMO}`,
+    };
+    const status = await onIssueComment(payload, users);
+    console.log(status);
+  } else {
+    console.log(0);
   }
 }
 
-onIssueComment();
+work();
