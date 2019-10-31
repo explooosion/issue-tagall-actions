b7a9e2eaa53d794af81bc3f91f3c60f805e84c70
robby <ta7382@gmail.com>
2019-10-31 15:32:01 +0800

    Fix auth, add create comment

--- index.js
+++ index.js
@@ -3,35 +3,34 @@ require('dotenv').config()
 const fs = require('fs')
 const Octokit = require('@octokit/rest')
 
-const octokit = new Octokit();
+const octokit = new Octokit({
+  auth: process.env.ACCESS_TOKEN
+});
 
-octokit.authenticate({
-  token: process.env.ACCESS_TOKEN
-})
+// octokit.authenticate({
+//   token: process.env.ACCESS_TOKEN
+// })
 
 async function onIssueComment() {
   console.log('GITHUB_EVENT_PATH', process.env.GITHUB_EVENT_PATH)
   const evetJSON = await JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'))
-  const eventCommnet = evetJSON.comment;
+  const OWNER = evetJSON.repository.user.login;
+  const REPOSITORY = evetJSON.repository.name;
+  const NUMBER = evetJSON.issue.number;
+  const Commnet = evetJSON.comment;
+
   console.log('Comment Payload', eventCommnet);
 
+  //add a comment to the issue
+  await octokit.issues.createComment({
+    owner: OWNER,
+    repo: REPOSITORY,
+    number: NUMBER,
+    body: `you said: ${Commnet.body}`,
+  }).then(({ data, headers, status }) => {
+    // handle data
+    console.log('BOT has send message.')
+  })
 }
 
 onIssueComment();
-
-// octokit.issues.listCommentsForRepo({
-//   owner: 'explooosion',
-//   repo: 'Agm-Direction',
-//   sort: 'created'
-// }).then(({ data }) => {
-//   console.log(data);
-// })
-
-// octokit.repos
-//   .listForOrg({
-//     org: "octokit",
-//     type: "public"
-//   })
-//   .then(({ data }) => {
-//     console.log(data);
-//   });
